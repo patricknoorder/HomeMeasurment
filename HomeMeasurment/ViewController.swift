8c47cd11fa09324dd5851b522202cae2732d5d2e
Patrick Noordermeer <patricknoordermeer@Patricks-MBP.fritz.box>
2015-05-04 16:58:50 +0200

    Initial Commit

---
+++ HomeMeasurment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeMeasurment
+//
+//  Created by Patrick Noordermeer on 04/05/15.
+//  Copyright (c) 2015 Patrick Noordermeer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
